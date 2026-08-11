16b21d5baa94f6ee8d9e7ede860197448773b17c
amruthamothadaka <amruthamothadaka29@gmail.com>
2026-08-11 12:27:57 +0530

    Add files via upload

---
+++ MyPack.java
@@ -0,0 +1,30 @@
+import java.util.scanner;
+class calculator{
+    addition()
+    subtraction()
+    multiplication()
+    division()
+}
+public class MyPack{
+    public static void main(String[] args){
+        scanner sc=new scanner();
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        public void addition(){
+            int sum=a+b
+            System.out.println("addition: "+sum)
+        }
+        public void subtraction(){
+            int minus=a-b
+            System.out.println("subtract: "+minus)
+        }
+        public void multiplication(){
+            int mul=a*b
+            System.out.println("addition: "+mul)
+        }
+        public void division(){
+            int div=a/b
+            System.out.println("addition: "+div)
+        }
+    }
+}
